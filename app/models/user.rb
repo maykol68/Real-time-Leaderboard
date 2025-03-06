6cca21af11a42afe9adba44dc0358f6fde1e8a1a
maykol68 <sm9365277@gmail.com>
2025-03-06 18:40:43 -0500

    i've added devise for auth

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  extend Devise::Models
+  include DeviseTokenAuth::Concerns::User
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable, :lockable
+end
